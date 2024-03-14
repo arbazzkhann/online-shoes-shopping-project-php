fdd5a603c1185a963b8feb099331360bb7ff30d9
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-03-14 18:39:30 +0530

    search page

---
+++ search.php
@@ -0,0 +1,90 @@
+<?php 
+    require 'header.php'; //Header
+    $str = mysqli_real_escape_string($conn, $_GET['str']);  // Storing id into variable
+
+    //condition if user put negative id in url
+    if($str != '' ) {
+        $get_product = get_product($conn, '', '', '', $str);  //calling getproduct function and storing into variable
+    }
+    else { ?>
+        <script>
+            window.location.href = "index.php";
+        </script>   
+    <?php }?>
+    
+<div class="body__overlay"></div>
+<!-- Start Bradcaump area -->
+<div class="ht__bradcaump__area"
+    style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+    <div class="ht__bradcaump__wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="bradcaump__inner">
+                        <nav class="bradcaump-inner">
+                            <a class="breadcrumb-item" href="index.php">Home</a>
+                            <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                            <span class="breadcrumb-item active">Search</span> 
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Bradcaump area -->
+<!-- Start Product Grid -->
+<section class="htc__product__grid bg__white ptb--100">
+    <div class="container">
+        <div class="row">
+            <?php 
+            if(count($get_product) > 0) {
+            ?>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="htc__product__rightidebar">
+                    <!-- Start Product View -->
+                    <div class="row">
+                        <div class="shop__grid__view__wrap">
+                            <div role="tabpanel" id="grid-view"
+                                class="single-grid-view tab-pane fade in active clearfix">
+                                <?php 
+                                    foreach($get_product as $list) { //loop for printing latest products
+                                ?>
+                                <!-- Start Single Category -->
+                                <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                                    <div class="category">
+                                        <div class="ht__cat__thumb">
+                                            <a href="product.php?id=<?php echo $list['id']?>">
+                                                <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>">
+                                            </a>
+                                        </div>
+
+                                        <div class="fr__product__inner">
+                                            <h4><a href="product-details.html"><?php echo $list['name']?></a></h4>
+                                            <ul class="fr__pro__prize">
+                                                <li class="old__prize">₹ <strike><?php echo $list['mrp']?></strike></li>
+                                                <li>₹ <?php echo $list['price']?></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php }?>
+                                <!-- End Single Category -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Product View -->
+                </div>
+                <?php 
+                } 
+                else {
+                    echo "NO RESULT FOUND!";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Product Grid -->
+<!-- End Banner Area -->
+<?php require 'footer.php'?>
