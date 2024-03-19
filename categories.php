c00ae8981e2442c58b6817bf29f3917cd19cf1f6
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-03-19 19:13:39 +0530

    changing href of name to product

--- categories.php
+++ categories.php
@@ -101,7 +101,7 @@
                                         </div>
 
                                         <div class="fr__product__inner">
-                                            <h4><a href="product-details.html"><?php echo $list['name']?></a></h4>
+                                            <h4><a href="product.php?id=<?php echo $list['id']?>"><?php echo $list['name']?></a></h4>
                                             <ul class="fr__pro__prize">
                                                 <li class="old__prize">₹ <strike><?php echo $list['mrp']?></strike></li>
                                                 <li>₹ <?php echo $list['price']?></li>
