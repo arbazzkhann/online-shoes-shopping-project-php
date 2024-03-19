187b98d554d4166ffd5f7c48a49da200701dcd46
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-03-19 17:07:02 +0530

    small changes

--- index.php
+++ index.php
@@ -65,7 +65,6 @@
             <div class="col-xs-12">
                 <div class="section__title--2 text-center">
                     <h2 class="title__line">New Arrivals</h2>
-                    <p>But I must explain to you how all this mistaken idea</p>
                 </div>
             </div>
         </div>
@@ -111,7 +110,6 @@
             <div class="col-xs-12">
                 <div class="section__title--2 text-center">
                     <h2 class="title__line">Best Seller</h2>
-                    <p>But I must explain to you how all this mistaken idea</p>
                 </div>
             </div>
         </div>
