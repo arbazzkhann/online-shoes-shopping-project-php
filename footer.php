dd8814217e718b0985eb538b56ae71e08b9adf03
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-03-19 17:05:07 +0530

    editing all footer links

--- footer.php
+++ footer.php
@@ -9,19 +9,16 @@
                             <div class="footer">
                                 <h2 class="title__line--2">ABOUT US</h2>
                                 <div class="ft__details">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna aliqua. Ut enim</p>
+                                    <p>"Welcome to Online Shoes Shopping, your one-stop destination for trendy footwear and unbeatable deals. Step into style today!"</p>
                                     <div class="ft__social__link">
                                         <ul class="social__link">
                                             <li><a href="#"><i class="icon-social-twitter icons"></i></a></li>
 
-                                            <li><a href="#"><i class="icon-social-instagram icons"></i></a></li>
+                                            <li><a href="https://www.instagram.com/xr_arbazsaifi/"><i class="icon-social-instagram icons"></i></a></li>
 
                                             <li><a href="#"><i class="icon-social-facebook icons"></i></a></li>
 
-                                            <li><a href="#"><i class="icon-social-google icons"></i></a></li>
-
-                                            <li><a href="#"><i class="icon-social-linkedin icons"></i></a></li>
+                                            <li><a href="https://www.linkedin.com/in/arbazzkhann/"><i class="icon-social-linkedin icons"></i></a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -35,7 +32,6 @@
                                 <div class="ft__inner">
                                     <ul class="ft__list">
                                         <li><a href="#">About us</a></li>
-                                        <li><a href="#">Delivery Information</a></li>
                                         <li><a href="#">Privacy & Policy</a></li>
                                         <li><a href="#">Terms & Condition</a></li>
                                         <li><a href="#">Manufactures</a></li>
@@ -50,44 +46,16 @@
                                 <h2 class="title__line--2">my account</h2>
                                 <div class="ft__inner">
                                     <ul class="ft__list">
-                                        <li><a href="#">My Account</a></li>
-                                        <li><a href="cart.html">My Cart</a></li>
-                                        <li><a href="#">Login</a></li>
-                                        <li><a href="wishlist.html">Wishlist</a></li>
-                                        <li><a href="checkout.html">Checkout</a></li>
+                                        <?php
+                                        if(isset($_SESSION['USER_LOGIN'])) {
+                                            echo '<li><a href="my_order.php">My Orders</a></li>';
+                                        } else {
+                                            echo '<li><a href="login.php">Create Account</a></li>';
+                                            echo '<li><a href="login.php">Login</a></li>';
+                                        }
+                                        ?>
+                                        <li><a href="cart.php">My Cart</a></li>
                                     </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Single Footer Widget -->
-                        <!-- Start Single Footer Widget -->
-                        <div class="col-md-2 col-sm-6 col-xs-12 xmt-40 smt-40">
-                            <div class="footer">
-                                <h2 class="title__line--2">Our service</h2>
-                                <div class="ft__inner">
-                                    <ul class="ft__list">
-                                        <li><a href="#">My Account</a></li>
-                                        <li><a href="cart.html">My Cart</a></li>
-                                        <li><a href="#">Login</a></li>
-                                        <li><a href="wishlist.html">Wishlist</a></li>
-                                        <li><a href="checkout.html">Checkout</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Single Footer Widget -->
-                        <!-- Start Single Footer Widget -->
-                        <div class="col-md-3 col-sm-6 col-xs-12 xmt-40 smt-40">
-                            <div class="footer">
-                                <h2 class="title__line--2">NEWSLETTER </h2>
-                                <div class="ft__inner">
-                                    <div class="news__input">
-                                        <input type="text" placeholder="Your Mail*">
-                                        <div class="send__btn">
-                                            <a class="fr__btn" href="#">Send Mail</a>
-                                        </div>
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
@@ -96,21 +64,6 @@
                 </div>
             </div>
             <!-- End Footer Widget -->
-            <!-- Start Copyright Area -->
-            <div class="htc__copyright bg__cat--5">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="copyright__inner">
-                                <p>Copyright© <a href="https://freethemescloud.com/">Free themes Cloud</a> 2018. All
-                                    right reserved.</p>
-                                <a href="#"><img src="images/others/shape/paypol.png" alt="payment images"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Copyright Area -->
         </footer>
         <!-- End Footer Style -->
         </div>
